4d49b02d9695ef530fa33edc6b88ce13465a4b3c
lm-costa <luism.costa00@gmail.com>
2024-01-30 16:46:39 -0300

    usethis create from git

--- R/arquivo_seila.R
+++ R/arquivo_seila.R
@@ -1,1 +1,3 @@
 ### comentario para o pedro
+usethis::create_from_github('pasta que qr clonar', destdir = 'para onde a pasta vai ser clonada',
+                            fork='FALSE se o rep não for seu')
